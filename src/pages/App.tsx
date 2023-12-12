9581ff87809e6410d749ee014c99e86dc4ca44fe
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-12-12 14:04:47 -0400

    feat: version one home

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,25 +10,27 @@ function App() {
     <Layout className='ateliartt-layout-aling'>
       <div className="ateliartt-butterfly" style={{ top: '100px', left: '50px' }}></div>
       <div className="ateliartt-butterfly" style={{ top: '200px', left: '150px' }}></div>
+      <div className="ateliartt-mesa" style={{ top: '270px', left: '150px' }}></div>
       <div className="ateliartt-butterfly" style={{ top: '500px', left: '1800px' }}></div>
       <div className="ateliartt-galho" style={{ top: '200px', left: '1500px' }}></div>
-      <div className="ateliartt-ramos" style={{ top: '600px', left: '100px' }}></div>
-      <div className="ateliartt-ramos" style={{ top: '600px', left: '700px' }}></div>
-      <div className="ateliartt-ramos" style={{ top: '600px', left: '1100px' }}></div>
+      <div className="ateliartt-kit" style={{ top: '600px', left: '100px' }}></div>
+      <div className="ateliartt-kit" style={{ top: '600px', left: '650px' }}></div>
+      <div className="ateliartt-kit" style={{ top: '600px', left: '1190px' }}></div>
       <Row className='ateliartt-row1-styles' justify="center" align="middle">
         <Row style={{ display: "block" }}>
           <Row justify="center">
-            <Typography.Title className="ateliartt-titlehome-styles">Bem Vindo!</Typography.Title>
+            <Typography.Title className="ateliartt-titlehome-styles animated-text">Bem Vindo!</Typography.Title>
           </Row>
           <Row style={{ marginTop: -150, marginBottom: -150, marginLeft: 200, marginRight: 200 }}>
             <Image
+              className="animated-text"
               height={700}
               src={logo}
               alt="logo ateliartt"
               preview={false}
             />
           </Row>
-          <Row justify="space-between">
+          <Row className="animated-text" justify="space-between">
             <Image
               className="ateliartt-image-styles"
               height={300}
