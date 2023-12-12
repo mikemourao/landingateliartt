41c1cbcdb68256d690f81e8a2b357f26990b35d0
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-12-12 16:31:31 -0400

    feat: animated scroll

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,34 @@
-import { Layout, Row, Image, Typography } from "antd";
+import { useEffect } from 'react';
+import { Layout, Row, Image, Typography, Card } from "antd";
 import logo from '../assets/logopersonalizados.svg'
 import safari from '../assets/imghomesafari.png'
 import boneca from '../assets/imghomeboneca.png'
 import mar from '../assets/imghomemar.png'
 import './styles.css'
+import Meta from "antd/es/card/Meta";
 
 function App() {
+  useEffect(() => {
+    const myObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    const elements = document.querySelectorAll('.hidden');
+
+    elements.forEach((element) => myObserver.observe(element));
+
+    // Limpar o observador quando o componente for desmontado
+    return () => {
+      elements.forEach((element) => myObserver.unobserve(element));
+    };
+  }, []);
+
   return (
     <Layout className='ateliartt-layout-aling'>
       <div className="ateliartt-butterfly" style={{ top: '100px', left: '50px' }}></div>
@@ -16,6 +39,7 @@ function App() {
       <div className="ateliartt-kit" style={{ top: '600px', left: '100px' }}></div>
       <div className="ateliartt-kit" style={{ top: '600px', left: '650px' }}></div>
       <div className="ateliartt-kit" style={{ top: '600px', left: '1190px' }}></div>
+      {/* home */}
       <Row className='ateliartt-row1-styles' justify="center" align="middle">
         <Row style={{ display: "block" }}>
           <Row justify="center">
@@ -53,6 +77,156 @@ function App() {
               preview={false}
             />
           </Row>
+        </Row>
+      </Row>
+      {/* produtos */}
+      <Row className="ateliartt-row2-styles">
+        <Row justify="center">
+          <Typography.Title className="ateliartt-titlehome-styles">Catálogo</Typography.Title>
+        </Row>
+        <Row justify="end">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="Safari"
+                  src={safari}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
+        </Row>
+        <Row justify="start">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="Circo"
+                  src={boneca}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
+        </Row>
+        <Row justify="end">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="Mar"
+                  src={mar}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
+        </Row>
+        <Row justify="start">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="Safari"
+                  src={safari}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
+        </Row>
+        <Row justify="end">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="Circo"
+                  src={boneca}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
+        </Row>
+        <Row justify="start">
+          <div className="hidden">
+            <Card
+              className='ateliartt-cardImg-styles'
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignContent: "center"
+              }}
+              cover={
+                <img
+                  height="100%"
+                  alt="mar"
+                  src={mar}
+                />
+              }
+              hoverable
+            >
+              <Meta
+                description="This is the description"
+              />
+            </Card>
+          </div>
         </Row>
       </Row>
     </Layout>
