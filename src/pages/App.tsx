7308c53bf5087d010d1193b61ca0ce6befa3b39f
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-12-26 17:06:22 -0400

    feat: media css

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { Layout, Row, Image, Typography, Card } from "antd";
 import logo from '../assets/logopersonalizados.svg'
+import what from '../assets/whatsapp.png'
+import instagram from '../assets/instagram.png'
 import safari from '../assets/imghomesafari.png'
 import boneca from '../assets/imghomeboneca.png'
 import mar from '../assets/imghomemar.png'
 import './styles.css'
-import Meta from "antd/es/card/Meta";
+import Meta from "antd/es/card/Meta";;
 
 function App() {
   useEffect(() => {
@@ -31,58 +33,81 @@ function App() {
 
   return (
     <Layout className='ateliartt-layout-aling'>
-      <div className="ateliartt-butterfly" style={{ top: '100px', left: '50px' }}></div>
-      <div className="ateliartt-butterfly" style={{ top: '200px', left: '150px' }}></div>
-      <div className="ateliartt-mesa" style={{ top: '270px', left: '150px' }}></div>
-      <div className="ateliartt-butterfly" style={{ top: '500px', left: '1800px' }}></div>
-      <div className="ateliartt-galho" style={{ top: '200px', left: '1500px' }}></div>
-      <div className="ateliartt-kit" style={{ top: '600px', left: '100px' }}></div>
-      <div className="ateliartt-kit" style={{ top: '600px', left: '650px' }}></div>
-      <div className="ateliartt-kit" style={{ top: '600px', left: '1190px' }}></div>
+      <div className="ateliartt-asteristic_blue" style={{ top: '16%', left: '46%' }}></div>
+      <div className="ateliartt-asteristic_pink" style={{ top: '82%', left: '75%' }}></div>
       {/* home */}
+      <Row className='ateliartt-rowHead-styles' justify="end" align="middle">
+        <div style={{ marginRight: 20 }}>
+          <Image
+            height={50}
+            src={instagram}
+            preview={false}
+          />
+        </div>
+        <div style={{ marginRight: 20 }}>
+          <Image
+            height={40}
+            src={what}
+            preview={false}
+          />
+        </div>
+      </Row>
       <Row className='ateliartt-row1-styles' justify="center" align="middle">
-        <Row style={{ display: "block" }}>
-          <Row justify="center">
-            <Typography.Title className="ateliartt-titlehome-styles animated-text">Bem Vindo!</Typography.Title>
-          </Row>
-          <Row style={{ marginTop: -150, marginBottom: -150, marginLeft: 200, marginRight: 200 }}>
-            <Image
-              className="animated-text"
-              height={700}
-              src={logo}
-              alt="logo ateliartt"
-              preview={false}
-            />
-          </Row>
-          <Row className="animated-text" justify="space-between">
-            <Image
-              className="ateliartt-image-styles"
-              height={300}
-              src={safari}
-              alt="safari"
-              preview={false}
-            />
-            <Image
-              className="ateliartt-image-styles"
-              height={300}
-              src={boneca}
-              alt="bonecas"
-              preview={false}
-            />
-            <Image
-              className="ateliartt-image-styles"
-              height={300}
-              src={mar}
-              alt="fundo do mar"
-              preview={false}
-            />
-          </Row>
-        </Row>
+        <div className="ateliartt-title" style={{ top: '20%', left: '12%' }}>
+          <Typography.Title
+            className="ateliartt-title-styles"
+          >
+            Ateliartt
+          </Typography.Title>
+        </div>
+      </Row>
+      <Row className='ateliartt-row1Body-styles' justify="center" align="middle">
+        <div className="ateliartt-title" style={{ top: '35%', left: '6%' }}>
+          <Typography
+            className="ateliartt-Subtitle-styles"
+          >
+            Papelaria Personalizada
+          </Typography>
+        </div>
+        <div className="ateliartt-img_safari" style={{ top: '20%', left: '50%' }}>
+          <Image
+            height={400}
+            src={safari}
+            alt="safari"
+            preview={false}
+          />
+        </div>
+        <div className="ateliartt-img_mar" style={{ top: '20%', left: '80%' }}>
+          <Image
+            height={250}
+            src={mar}
+            alt="safari"
+            preview={false}
+          />
+        </div>
+        <div className="ateliartt-img_boneca" style={{ top: '57%', left: '80%' }}>
+          <Image
+            height={250}
+            src={boneca}
+            alt="safari"
+            preview={false}
+          />
+        </div>
+        <div className="ateliartt-title" style={{ top: '55%', left: '5%' }}>
+          <Typography
+            className="ateliartt-about-styles"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+            Sed viverra, velit in tristique pharetra, erat massa convallis <br />
+            nisl, vitae eleifend tellus enim sit amet
+            libero. Cras rutrum aliquet molestie.
+          </Typography>
+        </div>
       </Row>
       {/* produtos */}
       <Row className="ateliartt-row2-styles">
         <Row justify="center">
-          <Typography.Title className="ateliartt-titlehome-styles">Catálogo</Typography.Title>
+          <Typography.Title className="ateliartt-title2-styles">Catálogo</Typography.Title>
         </Row>
         <Row justify="end">
           <div className="hidden">
